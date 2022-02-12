dd49af81df3025fe4406a42648e8cd044b68b179
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-02-12 13:45:02 +0100

    :FEAT: => Cours tableau

---
+++ index.js
@@ -0,0 +1,18 @@
+// ------ variable ----------
+var numero1 = 24;
+// declarer un type a une variable
+// le type declaré ne peut changé. un string, ne peut devenir un number. evite les erreurs
+var numero2;
+var numero3;
+var numero4;
+var numOrString;
+numOrString = 45;
+numOrString = '45';
+// NON numero2 = false;
+// OUI numero2 = 45;
+numero2 = 45;
+console.log(numero2);
+console.log(numOrString);
+// --------------------------
+// ------- Tableau ----------
+// --------------------------
---
+++ index.ts
@@ -0,0 +1,30 @@
+// ------ variable ----------
+
+let numero1 = 24;
+
+// declarer un type a une variable
+// le type declaré ne peut changé. un string, ne peut devenir un number. evite les erreurs
+let numero2:number;
+let numero3:string;
+let numero4:boolean;
+let numOrString: number | string;
+
+
+numOrString = 45;
+numOrString = '45';
+
+// NON numero2 = false;
+// OUI numero2 = 45;
+
+numero2 = 45;
+
+console.log(numero2);
+console.log(numOrString);
+
+// --------------------------
+
+// ------- Tableau ----------
+
+
+
+// --------------------------
